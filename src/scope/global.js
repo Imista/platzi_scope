91e58836f9e44139d8b44810755a8128e616e4d5
MuffinPlease <benjamintec3455@gmail.com>
2021-12-09 09:54:39 -0600

    Global.js - Bad practices

--- src/scope/global.js
+++ src/scope/global.js
@@ -1,2 +1,28 @@
-let hello = "HELLO";
-console.log(hello);
+var hello = "HELLO";
+var hello = "Bye";//Bad practice
+let world = "HELLO WORLD";
+const helloWorld = "Hello world!";
+
+const anotherFunction = () =>{
+    console.log(hello);
+    console.log(world);
+    console.log(helloWorld);
+}
+
+anotherFunction();
+
+//Bad practice-----------------------------
+const helloWorld = () => {
+    globalVar = "Im global";
+}
+
+helloWorld();
+console.log(globalVar);
+
+const anotherFunction = () =>{
+    localVar = globalVar = "Im global again";
+}
+
+anotherFunction();
+console.log(globalVar);
+//Bad practice------------------------------
