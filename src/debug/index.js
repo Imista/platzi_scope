ce21c406cef1ade62d0bc8515164b434c99d32d1
MuffinPlease <benjamintec3455@gmail.com>
2021-12-09 18:58:32 -0600

    Debugging

---
+++ src/debug/index.js
@@ -0,0 +1,32 @@
+var a = "Hello";
+
+function hello(){
+    let b = "Hello world";
+    const c = "Hello world!";
+    if(true){
+        let d = "Hello World!!";
+        debugger
+    }
+}
+
+hello();
+
+//
+
+const moneyBox = () => {
+    debugger
+    var saveCoins  = 0;
+    const countCoins = (coins) =>{
+        debugger
+        saveCoins += coins;        
+        console.log(`Money box: $${saveCoins}`);
+
+    }
+    return countCoins;
+}
+
+let myMoneyBox = moneyBox();
+
+myMoneyBox(4);
+myMoneyBox(6);
+myMoneyBox(10);
